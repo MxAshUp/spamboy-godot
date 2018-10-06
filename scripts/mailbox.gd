dc87f925b6aa6ea3241cbf564bd04eb2181d34c2
mangomago <markus.schneider84@gmail.com>
2018-10-06 13:17:15 +0200

    extended tilemap added sample level

---
+++ scripts/mailbox.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	$debug.hide()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
