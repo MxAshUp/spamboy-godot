619b246e8f011e6ce6dfa35dd9dfcd59df275773
Dustin Woods <pyrozzcom@gmail.com>
2018-10-05 22:56:21 -0700

    Walking physics

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,12 @@ export (int) var friction = 300
 export (int) var vertical_snap = 16
 export (int) var vertical_snap_speed = 310
 
+export (int) var walkling_acceleration = 400
+export (int) var walking_max_speed = 75
+export (int) var walking_friction = 600
+export (int) var walking_vertical_snap_speed = 200
+
+
 export (bool) var is_biking = false
 
 var velocity = Vector2()
@@ -20,6 +26,7 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	# @todo - it's more complicated than this
 	if Input.is_action_just_pressed("mount_bike"):
 		is_biking = !is_biking
 		
@@ -33,10 +40,10 @@ func _physics_process(delta):
 	var change_y_velocity = 0
 
 	if Input.is_action_pressed("ui_left"):
-		change_x_velocity -= acceleration
+		change_x_velocity -= 1
 
 	if Input.is_action_pressed("ui_right"):
-		change_x_velocity += acceleration
+		change_x_velocity += 1
 
 	if Input.is_action_pressed("ui_up"):
 		change_y_velocity -= 1
@@ -44,16 +51,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		change_y_velocity += 1
 
-  # update horizontal veloxity
+	change_x_velocity = change_x_velocity * (acceleration if is_biking else walkling_acceleration)
+  	# update horizontal veloxity
 	velocity.x += change_x_velocity * delta
 
 	# apply horizontal friction
 	if change_x_velocity == 0:
-		velocity.x -= min(abs(velocity.x), friction * delta) * sign(velocity.x)
+		velocity.x -= min(abs(velocity.x), (friction if is_biking else walking_friction) * delta) * sign(velocity.x)
 
 	# don't allow x velocity to exceed max speed 
-	if abs(velocity.x) > max_speed:
-		velocity.x = sign(velocity.x) * max_speed
+	if abs(velocity.x) > (max_speed if is_biking else walking_max_speed):
+		velocity.x = sign(velocity.x) * (max_speed if is_biking else walking_max_speed)
 
 	# decide which y position to snap to
 	if abs(change_y_velocity) > 0:
@@ -61,12 +69,14 @@ func _physics_process(delta):
 		if change_y_velocity > 0:
 			snapping_to_y += vertical_snap
 
-	# set vertical velocity based on which position to snap to
-	if abs(snapping_to_y - position.y) > 0:
-		velocity.y = (snapping_to_y - position.y) * vertical_snap_speed * min(max(0.06, abs(velocity.x) / max_speed), 0.5) * 2 * delta
 
+	# set vertical velocity based on which position to snap to
+	if is_biking:
+		if abs(snapping_to_y - position.y) > 0:
+			velocity.y = (snapping_to_y - position.y) * vertical_snap_speed * min(max(0.06, abs(velocity.x) / max_speed), 0.5) * 2 * delta
+	else:
+		if abs(snapping_to_y - position.y) > 0:
+			velocity.y = (snapping_to_y - position.y) * walking_vertical_snap_speed * delta		
 	# move player
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 
-func change_is_biking(new_is_biking):
-	is_biking = new_is_biking
