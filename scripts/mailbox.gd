d490c67bb501ec0310bbf188f59da484267252d2
mangomago <markus.schneider84@gmail.com>
2018-10-07 00:41:30 +0200

    added score node, animation and spawn trigger to mailbox.tscn

--- scripts/mailbox.gd
+++ scripts/mailbox.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var scoreScene = preload("res://scenes/scoreText.tscn")
 
 func _ready():
 	$debug.hide()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func feed():
+	$AnimationPlayer.play("feeded")
+	var score = scoreScene.instance()
+	score.position = $scoreSpawn.position
+	self.add_child(score)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		feed()
+
+func _on_Area2D_body_entered(body):
+	print("body entered: "+str(body))
