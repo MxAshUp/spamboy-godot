d00464c0903426dc2007cd3aaf0ecf0c50df4e1a
Dustin Woods <pyrozzcom@gmail.com>
2018-10-06 17:59:57 -0700

    comments

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
 func _ready():
+	# Connect all mailbox "feed" signals
 	for i in get_tree().get_nodes_in_group("mailbox"):
 		i.connect("feed", self, "handle_feed")
-	
+
+# todo - score and such
 func handle_feed():
 	$audioSamples.play("ui_good")
