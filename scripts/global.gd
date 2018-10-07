3340030a2809c6885651a1fc9e9363741a87be3a
Dustin Woods <pyrozzcom@gmail.com>
2018-10-07 14:59:03 -0700

    Cleanup - global doesn't care about score

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,21 +4,9 @@ enum GAMESTATE_ENUM { MAIN, CREDITS, GAME }
 
 var gameState = MAIN
 
-
-
-#Game Score
-var score = 0 
-#Elapsed Time on Level
-var elapsedTime = 0
-
-
-
 func _ready():
 	randomize()
-
-#Overwrite variables with defaults
+	
 func init():
-	score = 0
-
-
-#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
