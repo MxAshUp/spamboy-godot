e1c810fbe0331ed11e5e5500b5bdcd99a044cee7
Dustin Woods <pyrozzcom@gmail.com>
2018-10-07 22:13:14 -0700

    chase tweaks

--- scripts/door.gd
+++ scripts/door.gd
@@ -7,39 +7,41 @@ export(String, "NOTHING", "GRANDMA", "ANGRYMEN") var behindDoor = "NOTHING"
 
 var grumbleAtObject = null
 var grumbling = false
-
+export (float) var chase_speed = 10
 var chasing = false
-
+var move_direction = Vector2()
+			
 func _ready():
 	$doorSprite.set_frame(0)
 	$Area2D/angrymen.hide()
 	$Area2D/grandma.hide()
 
 func _process(delta):
-	if grumbleAtObject and grumbling:
-		grumbleAtObject.grumbled_at(delta)
-		if grumbleAtObject.position.x < ($Area2D.position + position).x:
-			$Area2D/angrymen.flip_h = true
-			$Area2D/grandma.flip_h = true
-		else:
-			$Area2D/angrymen.flip_h = false
-			$Area2D/grandma.flip_h = false
-		if chasing:
-			var move_offset = Vector2(0, -24)
-			var move_direction = Vector2()
-			move_direction = grumbleAtObject.position - ($Area2D.position + position + move_offset)
-			print(move_direction.y)
-			if abs(move_direction.y) > 5:
-				move_direction.x = 0
+	if grumbling:
+		if grumbleAtObject:
+			grumbleAtObject.grumbled_at(delta)
+			if grumbleAtObject.position.x < ($Area2D.position + position).x:
+				$Area2D/angrymen.flip_h = true
+				$Area2D/grandma.flip_h = true
+			else:
+				$Area2D/angrymen.flip_h = false
+				$Area2D/grandma.flip_h = false
+			if chasing:
+				var move_offset = Vector2(0, -24)
+				move_direction = Vector2()
+				move_direction = grumbleAtObject.position - ($Area2D.position + position + move_offset)
+
+		if abs(move_direction.y) > 5:
+			move_direction.x = 0
+		
+		if abs(move_direction.y) < 5 or abs($Area2D.position.y) > 24:
+			move_direction.y = 0
 			
-			if abs(move_direction.y) < 5 or abs($Area2D.position.y) > 64:
-				move_direction.y = 0
-				
-			if abs(move_direction.x) < 5 or abs($Area2D.position.x) > 8:
-				move_direction.x = 0
-				
-			move_direction = move_direction.normalized() * 10
-			$Area2D.position += move_direction * delta
+		if abs(move_direction.x) < 5 or abs($Area2D.position.x) > 8:
+			move_direction.x = 0
+			
+		move_direction = move_direction.normalized() * chase_speed
+		$Area2D.position += move_direction * delta
 
 #TODO maybe animation
 func openDoor():
